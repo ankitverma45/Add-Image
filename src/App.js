2ee2e5aae920d9728a3660d66e4133457fe7a6aa
ankit <kumarankit14121999@gmail.com>
2022-10-30 16:27:29 +0530

    image-website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [imageList, setImageList] = useState([
+    { url: "https://source.unsplash.com/random/1" },
+    { url: "https://source.unsplash.com/random/2" },
+    { url: "https://source.unsplash.com/random/3" },
+    { url: "https://source.unsplash.com/random/4" },
+    { url: "https://source.unsplash.com/random/5" },
+    { url: "https://source.unsplash.com/random/6" },
+  ]);
+  console.log(imageList);
+
+  const getNewImages = () => {
+    setImageList([
+      ...imageList,
+      {
+        url: `https://source.unsplash.com/random/${Math.floor(
+          Math.random() * 100
+        )}`,
+      },
+    ]);
+  };
+  const getremoveImage = () => {
+    imageList.pop();
+    console.log(imageList);
+    setImageList([...imageList]);
+  };
+
+  const showImage = imageList.map((value) => {
+    const background=value.url
+    return (
+      <div className="cardImage" style={{ backgroundImage: `url(${background})` }}>
+      </div>
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="header">
+        <div className="cardHeader">{showImage}</div>
+        <div className="buttonContainer">
+          <button onClick={getremoveImage}>Remove</button>
+          <button onClick={getNewImages}>Add</button>
+        </div>
+      </section>
+    </>
   );
 }
 
